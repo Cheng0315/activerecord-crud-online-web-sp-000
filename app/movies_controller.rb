9f4c628575519a275595192104e6c7efc0ecf93b
Cheng Vue <cvu3@ucsc.edu>
2018-12-28 23:16:47 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -31,12 +31,9 @@ def can_be_created_in_a_block(args = {})
   # release_date == 1990
 
   Movie.create do |m|
-    if !args.empty?
-      args.each {|k, v| m[k] = v}
-    else
+
       m.title = "Home Alone"
       m.release_date = 1990
-    end
   end
 end
 
