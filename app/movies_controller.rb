5c0d28f3dc3462f09bbdb280f44b9284fb0ca8ee
Cheng Vue <cvu3@ucsc.edu>
2018-12-29 00:16:14 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -67,7 +67,7 @@ end
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
-  Movie.find_all(release_date: 2000)
+  Movie.all(release_date: 2000)
 end
 
 def can_be_found_updated_and_saved
